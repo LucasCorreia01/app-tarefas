1f79d2d6c33fb9687f7e773090ecc495c410d73c
LucasCorreia01 <lucascorreiadamaceno@gmail.com>
2023-07-20 17:22:26 -0300

    finalizada a autenticação

--- lib/services/http_interceptor.dart
+++ lib/services/http_interceptor.dart
@@ -7,21 +7,21 @@ class LoggingInterceptor implements InterceptorContract {
   Logger logger = Logger(printer: PrettyPrinter(methodCount: 0));
   @override
   Future<RequestData> interceptRequest({required RequestData data}) async {
-    // logger.v(
-    //     'Cabeçalhos:${data.headers}\nStatus Code:${data.baseUrl}\nCorpo:${data.body}');
+    logger.v(
+        'Cabeçalhos:${data.headers}\nStatus Code:${data.baseUrl}\nCorpo:${data.body}');
     return data;
   }
 
   @override
   Future<ResponseData> interceptResponse({required ResponseData data}) async {
 
-    // if(data.statusCode ~/ 100 == 2){
-    //   logger.i(
-    //     'Cabeçalhos:${data.headers}\nStatus Code:${data.statusCode}\nCorpo:${data.body}');
-    // } else {
-    //   logger.e(
-    //     'Cabeçalhos:${data.headers}\nStatus Code:${data.statusCode}\nCorpo:${data.body}');
-    // }
+    if(data.statusCode ~/ 100 == 2){
+      logger.i(
+        'Cabeçalhos:${data.headers}\nStatus Code:${data.statusCode}\nCorpo:${data.body}');
+    } else {
+      logger.e(
+        'Cabeçalhos:${data.headers}\nStatus Code:${data.statusCode}\nCorpo:${data.body}');
+    }
     
 
     return data;
